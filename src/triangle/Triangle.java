936748aa661754889278d85aa92c2d8f6193e7f7
GoldyLocksV <valentin@krolls.net>
2025-06-04 20:17:50 +0200

    Recursive Triangles+Random Color

--- src/triangle/Triangle.java
+++ src/triangle/Triangle.java
@@ -5,6 +5,8 @@ import resizable.ResizableImage;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.lang.Math.*;
+import java.util.Random;
+
 import static resizable.Debug.print;
 
 /**
@@ -22,6 +24,7 @@ import static resizable.Debug.print;
  */
 public class Triangle implements ResizableImage {
     int drawTriangle = 0;
+    int IterationDepth = 0;
     /**
      * change this method to implement the triangle!
      * @param size the outer bounds of the triangle
@@ -32,26 +35,36 @@ public class Triangle implements ResizableImage {
         BufferedImage bufferedImage = new BufferedImage(size.width, size.height, BufferedImage.TYPE_INT_ARGB);
         Graphics2D gBuffer = (Graphics2D) bufferedImage.getGraphics();
         gBuffer.setColor(Color.gray);
+
         if((int)((Math.sqrt(3)*size.width)/2) < size.height) {
-            int[] xPoints = {0, size.width / 2, size.width};
-            int[] yPoints ={0, (int) ((Math.sqrt(3) * size.width) / 2), 0};
-            drawTriangle(gBuffer, xPoints, yPoints);
+            int[] xStart = {0, size.width / 2, size.width};
+            int[] yStart ={0, (int) ((Math.sqrt(3) * size.width) / 2), 0};
+            drawTriangle(gBuffer, xStart, yStart);
         }
         else {
             int halfLength = (int)((2*size.height)/Math.sqrt(3))/2;
-            int[] xPoints = {(size.width/2)- halfLength,size.width/2,(size.width/2)+ halfLength};
-            int[] yPoints ={0,size.height,0};
-            drawTriangle(gBuffer, xPoints, yPoints);
+            int[] xStart = {(size.width/2)- halfLength,size.width/2,(size.width/2)+ halfLength};
+            int[] yStart ={0,size.height,0};
+            drawTriangle(gBuffer, xStart, yStart);
         }
         return bufferedImage;
     }
 
 
 
-    private void drawTriangle(Graphics2D gBuffer, int xPoints[], int[] yPoints){
+    private void drawTriangle(Graphics2D gBuffer, int[] xPoints, int[] yPoints){
+        gBuffer.setColor(new Color((int)(Math.random()*255),(int)(Math.random()*255),(int)(Math.random()*255)));
         gBuffer.fillPolygon(xPoints, yPoints, 3);
-
+        if (IterationDepth < 3) {
+            IterationDepth++;
+            drawTriangle(gBuffer, new int[]{xPoints[0], (xPoints[0]+xPoints[1]) / 2, (xPoints[0]+xPoints[2]) / 2}, new int[]{yPoints[0], (yPoints[0]+yPoints[1])/ 2, yPoints[0]});
+            drawTriangle(gBuffer, new int[]{(xPoints[0]+xPoints[1])/2, xPoints[1], (xPoints[1]+xPoints[2]) / 2}, new int[]{(yPoints[0]+yPoints[1])/2,yPoints[1],(yPoints[0]+yPoints[1])/2});
+            drawTriangle(gBuffer, new int[]{(xPoints[0]+xPoints[2])/2,(xPoints[1]+xPoints[2])/2,xPoints[2]}, new int[]{yPoints[0], (yPoints[0]+yPoints[1])/ 2, yPoints[0]});
+            IterationDepth--;
+        }
     }
+
+
     BufferedImage bufferedImage;
     Dimension bufferedImageSize;
 
